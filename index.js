1ee029b1587fc5345bd96b6ea7ce97323fe875e9
Raymond Harvey <raymh2002@aol.com>
2018-01-19 21:27:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much."
+function resetKittens() {
+  var kittens = new Array("Milo", "Otis", "Garfield");
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  resetKittens();
+}
